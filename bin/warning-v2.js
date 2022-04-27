fa8402172629bf7a0d5adde05842b873a4342d68
b-ma <benjamin.matuszewski@gmail.com>
2022-04-27 18:12:55 +0200

    improved warning

--- bin/warning-v2.js
+++ bin/warning-v2.js
@@ -6,6 +6,8 @@ console.warn(`
 > Since v2, this package has moved to lit@2, which might cause issues if your
 > application is using lit-html@1.x.x and/or lit-element@1.x.x
 
-> if you don't want to update your app now, please fall back to version 1.3.0
+> to update your app, see: https://lit.dev/docs/releases/upgrade/
+
+> if you don't want to update your app now, please fall back to version 1.x.x
 \`npm install --save @ircam/simple-components@1.3.0\`
 `);
