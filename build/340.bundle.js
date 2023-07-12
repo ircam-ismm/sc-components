945a4d25dfc5cedaf09e568546c17c9e08526345
b-ma <b-ma@users.noreply.github.com>
2023-07-12 10:39:32 +0000

    Deploying to gh-pages from @ ircam-ismm/sc-components@61a4f555d6e18b6c829634c4cef2d5ea2901e366 🚀

--- build/340.bundle.js
+++ build/340.bundle.js
@@ -1,116 +1,112 @@
 "use strict";(self.webpackChunk_ircam_sc_components_doc=self.webpackChunk_ircam_sc_components_doc||[]).push([[340],{1340:(e,t,s)=>{s.r(t),s.d(t,{template:()=>d});var c=s(9392),o=s(1142),a=s(4670);const d=c.dy`
 
-<div class="example">
-  <h2>sc-dots</h2>
+<h2>sc-dots</h2>
 
-  <sc-code-example language="javascript">${"\n  import { html } from 'lit';\n  import '@ircam/sc-components/sc-dots.js';\n\n  const template = html`<sc-dots></sc-dots>`;\n  "}</sc-code-example>
+<sc-code-example language="javascript">${"\nimport { html } from 'lit';\nimport '@ircam/sc-components/sc-dots.js';\n\nconst template = html`<sc-dots></sc-dots>`;\n"}</sc-code-example>
 
-  <sc-dots
-    id="test-dots"
-    .value=${[{x:.5,y:.5}]}
-    @input=${e=>document.querySelector("#dots-input").value=o.stringify(e.detail.value,null,2)}
-  ></sc-dots>
+<sc-dots
+  id="test-dots"
+  .value=${[{x:.5,y:.5}]}
+  @input=${e=>document.querySelector("#dots-input").value=o.stringify(e.detail.value,null,2)}
+></sc-dots>
+
+<h3>Properties</h3>
+
+<div>
+  <p>dot positions must contain the "x" and "y" fields</p>
+  <sc-text readonly>[.value=[]]</sc-text>
+  <sc-text
+    style="width: 300px;"
+    @change=${e=>document.querySelector("#test-dots").value=o.parse(e.detail.value)}
+  >[{ x: 0.5, y: 0.5 }]</sc-text>
+</div>
+<div>
+  <p>an optionnal "color" field can be given</p>
+  <sc-text readonly>[.value=[]]</sc-text>
+  <sc-text
+    style="width: 300px;"
+    @change=${e=>document.querySelector("#test-dots").value=o.parse(e.detail.value)}
+  >[{ x: 0.5, y: 0.5, color: 'red' }]</sc-text>
 </div>
 
-<div class="api">
-  <h3>Properties</h3>
+<h3>Attributes</h3>
 
-  <div>
-    <p>dot positions must contain the "x" and "y" fields</p>
-    <sc-text readonly>[.value=[]]</sc-text>
-    <sc-text
-      style="width: 300px;"
-      @change=${e=>document.querySelector("#test-dots").value=o.parse(e.detail.value)}
-    >[{ x: 0.5, y: 0.5 }]</sc-text>
-  </div>
-  <div>
-    <p>an optionnal "color" field can be given</p>
-    <sc-text readonly>[.value=[]]</sc-text>
-    <sc-text
-      style="width: 300px;"
-      @change=${e=>document.querySelector("#test-dots").value=o.parse(e.detail.value)}
-    >[{ x: 0.5, y: 0.5, color: 'red' }]</sc-text>
-  </div>
+<div>
+  <sc-text readonly>[x-range=[0, 1]]</sc-text>
+  <sc-text
+    @change=${e=>document.querySelector("#test-dots").xRange=o.parse(e.detail.value)}
+  >[0, 1]</sc-text>
+</div>
 
-  <h3>Attributes</h3>
+<div>
+  <sc-text readonly>[y-range=[0, 1]]</sc-text>
+  <sc-text
+    @change=${e=>document.querySelector("#test-dots").yRange=o.parse(e.detail.value)}
+  >[0, 1]</sc-text>
+</div>
 
-  <div>
-    <sc-text readonly>[x-range=[0, 1]]</sc-text>
-    <sc-text
-      @change=${e=>document.querySelector("#test-dots").xRange=o.parse(e.detail.value)}
-    >[0, 1]</sc-text>
-  </div>
+<div>
+  <p>radius of the dots in pixels (if set, takes precedence over "radius-relative")</p>
+  <sc-text readonly>[radius=5]</sc-text>
+  <sc-slider
+    min="5"
+    max="100"
+    @input=${e=>document.querySelector("#test-dots").radius=e.detail.value}
+  >[0, 1]</sc-slider>
+</div>
+<div>
+  <p>radius relative to the given ranges</p>
+  <sc-text readonly>[radius-relative=null]</sc-text>
+  <sc-slider
+    @input=${e=>{document.querySelector("#test-dots").radius=null,document.querySelector("#test-dots").radiusRelative=e.detail.value}}
+  ></sc-slider>
+</div>
 
-  <div>
-    <sc-text readonly>[y-range=[0, 1]]</sc-text>
-    <sc-text
-      @change=${e=>document.querySelector("#test-dots").yRange=o.parse(e.detail.value)}
-    >[0, 1]</sc-text>
-  </div>
+<div style="margin-top: 30px;">
+  <p>use sc-dot component as (multitouch) input interface</p>
+  <sc-text readonly>[?capture-events=false]</sc-text>
+  <sc-toggle
+    @change=${e=>document.querySelector("#test-dots").captureEvents=e.detail.value}
+  ></sc-toggle>
+</div>
+<div>
+  <p>"persist-events" only works if "capture-events" is set</p>
+  <sc-text readonly>[?persist-events=false]</sc-text>
+  <sc-toggle
+    @change=${e=>document.querySelector("#test-dots").persistEvents=e.detail.value}
+  ></sc-toggle>
 
-  <div>
-    <p>radius of the dots in pixels (if set, takes precedence over "radius-relative")</p>
-    <sc-text readonly>[radius=5]</sc-text>
-    <sc-slider
-      min="5"
-      max="100"
-      @input=${e=>document.querySelector("#test-dots").radius=e.detail.value}
-    >[0, 1]</sc-slider>
-  </div>
-  <div>
-    <p>radius relative to the given ranges</p>
-    <sc-text readonly>[radius-relative=null]</sc-text>
-    <sc-slider
-      @input=${e=>{document.querySelector("#test-dots").radius=null,document.querySelector("#test-dots").radiusRelative=e.detail.value}}
-    ></sc-slider>
-  </div>
+<h3>Events</h3>
 
-  <div style="margin-top: 30px;">
-    <p>use sc-dot component as (multitouch) input interface</p>
-    <sc-text readonly>[?capture-events=false]</sc-text>
-    <sc-toggle
-      @change=${e=>document.querySelector("#test-dots").captureEvents=e.detail.value}
-    ></sc-toggle>
-  </div>
-  <div>
-    <p>"persist-events" only works if "capture-events" is set</p>
-    <sc-text readonly>[?persist-events=false]</sc-text>
-    <sc-toggle
-      @change=${e=>document.querySelector("#test-dots").persistEvents=e.detail.value}
-    ></sc-toggle>
+<div>
+  <p>set "capture-events" to true to get events from the interface</p>
+  <sc-text readonly>[@input]</sc-text>
+  <sc-text id="dots-input" readonly style="height: 200px;"></sc-text>
 
-  <h3>Events</h3>
+</div>
 
-  <div>
-    <p>set "capture-events" to true to get events from the interface</p>
-    <sc-text readonly>[@input]</sc-text>
-    <sc-text id="dots-input" readonly style="height: 200px;"></sc-text>
+<h3>Styling</h3>
+<sc-editor
+  save-button
+  style="width: 500px;"
+  value="\
+#test-dots {
+  width: 300px;
+  height: 300px;
+  background-color: var(--sc-color-primary-1);
 
-  </div>
+  --sc-dots-opacity: 1;
+  --sc-dots-color: var(--sc-color-secondary-2);
+  --sc-dots-background-image: url(./assets/seating-map.png);
+  --sc-dots-background-color: var(--sc-color-primary-1);
+}
+  "
+  @change=${e=>(0,a.default)(e.detail.value)}
+></sc-editor>
 
-  <h3>Styling</h3>
-  <sc-editor
-    save-button
-    style="width: 500px;"
-    value="\
-  #test-dots {
-    width: 300px;
-    height: 300px;
-    background-color: var(--sc-color-primary-1);
-
-    --sc-dots-opacity: 1;
-    --sc-dots-color: var(--sc-color-secondary-2);
-    --sc-dots-background-image: url(./assets/seating-map.png);
-    --sc-dots-background-color: var(--sc-color-primary-1);
-  }
-    "
-    @change=${e=>(0,a.default)(e.detail.value)}
-  ></sc-editor>
-
-  <div>
-    <sc-text readonly>debug containers</sc-text>
-    <sc-toggle @change=${e=>document.querySelector("#test-dots").classList.toggle("debug")}></sc-toggle>
-  </div>
+<div>
+  <sc-text readonly>debug containers</sc-text>
+  <sc-toggle @change=${e=>document.querySelector("#test-dots").classList.toggle("debug")}></sc-toggle>
 </div>
 
 `}}]);
--- src/sc-dots.js
+++ src/sc-dots.js
@@ -28,125 +28,121 @@ import applyStyle from './utils/applyStyle.js';
 
 export const template = html`
 
-<div class="example">
-  <h2>sc-dots</h2>
+<h2>sc-dots</h2>
 
-  <sc-code-example language="javascript">${`
-  import { html } from 'lit';
-  import '@ircam/sc-components/sc-dots.js';
+<sc-code-example language="javascript">${`
+import { html } from 'lit';
+import '@ircam/sc-components/sc-dots.js';
 
-  const template = html\`<sc-dots></sc-dots>\`;
-  `}</sc-code-example>
+const template = html\`<sc-dots></sc-dots>\`;
+`}</sc-code-example>
 
-  <sc-dots
-    id="test-dots"
-    .value=${[{ x: 0.5, y: 0.5 }]}
-    @input=${e => document.querySelector('#dots-input').value = JSON5.stringify(e.detail.value, null, 2)}
-  ></sc-dots>
+<sc-dots
+  id="test-dots"
+  .value=${[{ x: 0.5, y: 0.5 }]}
+  @input=${e => document.querySelector('#dots-input').value = JSON5.stringify(e.detail.value, null, 2)}
+></sc-dots>
+
+<h3>Properties</h3>
+
+<div>
+  <p>dot positions must contain the "x" and "y" fields</p>
+  <sc-text readonly>[.value=[]]</sc-text>
+  <sc-text
+    style="width: 300px;"
+    @change=${e => document.querySelector('#test-dots').value = JSON5.parse(e.detail.value)}
+  >[{ x: 0.5, y: 0.5 }]</sc-text>
+</div>
+<div>
+  <p>an optionnal "color" field can be given</p>
+  <sc-text readonly>[.value=[]]</sc-text>
+  <sc-text
+    style="width: 300px;"
+    @change=${e => document.querySelector('#test-dots').value = JSON5.parse(e.detail.value)}
+  >[{ x: 0.5, y: 0.5, color: 'red' }]</sc-text>
 </div>
 
-<div class="api">
-  <h3>Properties</h3>
+<h3>Attributes</h3>
 
-  <div>
-    <p>dot positions must contain the "x" and "y" fields</p>
-    <sc-text readonly>[.value=[]]</sc-text>
-    <sc-text
-      style="width: 300px;"
-      @change=${e => document.querySelector('#test-dots').value = JSON5.parse(e.detail.value)}
-    >[{ x: 0.5, y: 0.5 }]</sc-text>
-  </div>
-  <div>
-    <p>an optionnal "color" field can be given</p>
-    <sc-text readonly>[.value=[]]</sc-text>
-    <sc-text
-      style="width: 300px;"
-      @change=${e => document.querySelector('#test-dots').value = JSON5.parse(e.detail.value)}
-    >[{ x: 0.5, y: 0.5, color: 'red' }]</sc-text>
-  </div>
+<div>
+  <sc-text readonly>[x-range=[0, 1]]</sc-text>
+  <sc-text
+    @change=${e => document.querySelector('#test-dots').xRange = JSON5.parse(e.detail.value)}
+  >[0, 1]</sc-text>
+</div>
 
-  <h3>Attributes</h3>
+<div>
+  <sc-text readonly>[y-range=[0, 1]]</sc-text>
+  <sc-text
+    @change=${e => document.querySelector('#test-dots').yRange = JSON5.parse(e.detail.value)}
+  >[0, 1]</sc-text>
+</div>
 
-  <div>
-    <sc-text readonly>[x-range=[0, 1]]</sc-text>
-    <sc-text
-      @change=${e => document.querySelector('#test-dots').xRange = JSON5.parse(e.detail.value)}
-    >[0, 1]</sc-text>
-  </div>
+<div>
+  <p>radius of the dots in pixels (if set, takes precedence over "radius-relative")</p>
+  <sc-text readonly>[radius=5]</sc-text>
+  <sc-slider
+    min="5"
+    max="100"
+    @input=${e => document.querySelector('#test-dots').radius = e.detail.value}
+  >[0, 1]</sc-slider>
+</div>
+<div>
+  <p>radius relative to the given ranges</p>
+  <sc-text readonly>[radius-relative=null]</sc-text>
+  <sc-slider
+    @input=${e => {
+      document.querySelector('#test-dots').radius = null;
+      document.querySelector('#test-dots').radiusRelative = e.detail.value
+    }}
+  ></sc-slider>
+</div>
 
-  <div>
-    <sc-text readonly>[y-range=[0, 1]]</sc-text>
-    <sc-text
-      @change=${e => document.querySelector('#test-dots').yRange = JSON5.parse(e.detail.value)}
-    >[0, 1]</sc-text>
-  </div>
+<div style="margin-top: 30px;">
+  <p>use sc-dot component as (multitouch) input interface</p>
+  <sc-text readonly>[?capture-events=false]</sc-text>
+  <sc-toggle
+    @change=${e => document.querySelector('#test-dots').captureEvents = e.detail.value}
+  ></sc-toggle>
+</div>
+<div>
+  <p>"persist-events" only works if "capture-events" is set</p>
+  <sc-text readonly>[?persist-events=false]</sc-text>
+  <sc-toggle
+    @change=${e => document.querySelector('#test-dots').persistEvents = e.detail.value}
+  ></sc-toggle>
 
-  <div>
-    <p>radius of the dots in pixels (if set, takes precedence over "radius-relative")</p>
-    <sc-text readonly>[radius=5]</sc-text>
-    <sc-slider
-      min="5"
-      max="100"
-      @input=${e => document.querySelector('#test-dots').radius = e.detail.value}
-    >[0, 1]</sc-slider>
-  </div>
-  <div>
-    <p>radius relative to the given ranges</p>
-    <sc-text readonly>[radius-relative=null]</sc-text>
-    <sc-slider
-      @input=${e => {
-        document.querySelector('#test-dots').radius = null;
-        document.querySelector('#test-dots').radiusRelative = e.detail.value
-      }}
-    ></sc-slider>
-  </div>
+<h3>Events</h3>
 
-  <div style="margin-top: 30px;">
-    <p>use sc-dot component as (multitouch) input interface</p>
-    <sc-text readonly>[?capture-events=false]</sc-text>
-    <sc-toggle
-      @change=${e => document.querySelector('#test-dots').captureEvents = e.detail.value}
-    ></sc-toggle>
-  </div>
-  <div>
-    <p>"persist-events" only works if "capture-events" is set</p>
-    <sc-text readonly>[?persist-events=false]</sc-text>
-    <sc-toggle
-      @change=${e => document.querySelector('#test-dots').persistEvents = e.detail.value}
-    ></sc-toggle>
+<div>
+  <p>set "capture-events" to true to get events from the interface</p>
+  <sc-text readonly>[@input]</sc-text>
+  <sc-text id="dots-input" readonly style="height: 200px;"></sc-text>
 
-  <h3>Events</h3>
+</div>
 
-  <div>
-    <p>set "capture-events" to true to get events from the interface</p>
-    <sc-text readonly>[@input]</sc-text>
-    <sc-text id="dots-input" readonly style="height: 200px;"></sc-text>
+<h3>Styling</h3>
+<sc-editor
+  save-button
+  style="width: 500px;"
+  value="\
+#test-dots {
+  width: 300px;
+  height: 300px;
+  background-color: var(--sc-color-primary-1);
 
-  </div>
+  --sc-dots-opacity: 1;
+  --sc-dots-color: var(--sc-color-secondary-2);
+  --sc-dots-background-image: url(./assets/seating-map.png);
+  --sc-dots-background-color: var(--sc-color-primary-1);
+}
+  "
+  @change=${e => applyStyle(e.detail.value)}
+></sc-editor>
 
-  <h3>Styling</h3>
-  <sc-editor
-    save-button
-    style="width: 500px;"
-    value="\
-  #test-dots {
-    width: 300px;
-    height: 300px;
-    background-color: var(--sc-color-primary-1);
-
-    --sc-dots-opacity: 1;
-    --sc-dots-color: var(--sc-color-secondary-2);
-    --sc-dots-background-image: url(./assets/seating-map.png);
-    --sc-dots-background-color: var(--sc-color-primary-1);
-  }
-    "
-    @change=${e => applyStyle(e.detail.value)}
-  ></sc-editor>
-
-  <div>
-    <sc-text readonly>debug containers</sc-text>
-    <sc-toggle @change=${e => document.querySelector('#test-dots').classList.toggle('debug')}></sc-toggle>
-  </div>
+<div>
+  <sc-text readonly>debug containers</sc-text>
+  <sc-toggle @change=${e => document.querySelector('#test-dots').classList.toggle('debug')}></sc-toggle>
 </div>
 
 `;
