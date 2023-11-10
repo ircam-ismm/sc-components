a9e7117a4620d2ff009f6aef57fd0714a95bdbf6
b-ma <benjamin.matuszewski@gmail.com>
2023-11-10 07:37:22 +0100

    wip(sc-filter): use raw interactions

--- src/sc-filter.js
+++ src/sc-filter.js
@@ -48,18 +48,13 @@ class ScFilter extends ScElement {
     svg {
       width: 100%;
       height: 100%;
+      position: relative;
     }
 
     .ruler {
       stroke: var(--sc-color-secondary-1);
-      stroke-opacity: 0.5;
+      stroke-opacity: 0.3;
       stroke-width: 1px;
-    }
-
-    .filter-control {
-      stroke: var(--sc-color-secondary-2);
-      stroke-opacity: 1;
-      stroke-width: 3;
     }
 
     .accumulated-filter {
@@ -71,24 +66,53 @@ class ScFilter extends ScElement {
     .selected-filter {
       fill: none;
       stroke: var(--sc-color-primary-5);
-      stroke-width: 3;
+      stroke-width: 1;
     }
 
-    sc-position-surface {
-      width: 100%;
-      height: 100%;
-      position: absolute;
-      top: 0;
-      left: 0;
+    .filter-controls rect {
+      fill: white;
+      fill-opacity: 0;
+    }
+
+    .filter-controls:hover rect {
+      fill: white;
+      fill-opacity: 0.1;
+      cursor: move;
+    }
+
+    .filter-controls line {
+      stroke: var(--sc-color-secondary-2);
+      stroke-opacity: 1;
+      stroke-width: 1;
+    }
+
+    .filter-controls:hover line {
+      cursor: move;
+    }
+
+    .filter-controls line.q-control {
+      cursor: ew-resize;
     }
   `;
 
   constructor() {
     super();
 
-    this.numFilters = 2;
+    this.numFilters = 1;
     this.editFilterIndex = 0;
     this.mode = 'log';
+
+    this.minGain = -24;
+    this.maxGain = 24;
+
+    this._width = 300;
+    this._height = 150;
+
+    this._f1 = null;
+    this._f2 = null;
+
+    // f0 (f0 + gain) | f1 (Q + gain) | f2 (Q + gain)
+    this._editMode = null;
 
     // @todo - this should be dynamic too
     this._filterNodes = [];
@@ -102,10 +126,10 @@ class ScFilter extends ScElement {
     this._filterNodes[0].gain.value = +6;
     this._filterNodes[0].Q.value = 0.2;
 
-    this._filterNodes[1].type = 'highshelf';
-    this._filterNodes[1].frequency.value = 2000;
-    this._filterNodes[1].gain.value = -12;
-    this._filterNodes[1].Q.value = 0.3;
+    // this._filterNodes[1].type = 'highshelf';
+    // this._filterNodes[1].frequency.value = 2000;
+    // this._filterNodes[1].gain.value = -12;
+    // this._filterNodes[1].Q.value = 0.3;
     // @todo - this should dynamic according to sample rate
 
     // number of computed points to display the reponse curve
@@ -134,23 +158,27 @@ class ScFilter extends ScElement {
     this._selectedFilterDbResponse = new Float32Array(numPoints)
     // response accumulated for all filters
     this._accumulatedDbResponse = new Float32Array(numPoints);
+
+
+    this._onMouseMove = this._onMouseMove.bind(this);
+    this._onMouseUp = this._onMouseUp.bind(this);
   }
 
   render() {
     const freqs = this.mode === 'log' ? this._frequencyHzLog : this._frequencyHzLin;
 
     // gain only change something for certain filter types, i.e. lowshelf, highshelf, peaking
-    this._filterNodes[0].type = 'peaking';
+    // this._filterNodes[0].type = 'peaking';
     // this._filterNodes[0].gain.value = 1;
     // this._filterNodes[0].Q.value = 3;
-
-    // console.log(this._filterNodes[0].gain.value)
 
     // reset accumulated buffer
     for (let i = 0; i < this._accumulatedDbResponse.length; i++) {
       this._accumulatedDbResponse[i] = 0;
     }
 
+    // - compute filters in dB according to their frequency response
+    // - compute accumulated filter
     this._filterNodes.forEach((filterNode, filterIndex) => {
       // set filter node
       filterNode.getFrequencyResponse(freqs, this._magResponse, this._phaseResponse);
@@ -167,15 +195,11 @@ class ScFilter extends ScElement {
       }
     });
 
-    // @todo - work with real width and height so that lines width are consistant
-    const minDB = -24;
-    const maxDB = +24;
-
-    const xStep = 1000 / (freqs.length - 1);
-    const yScale = linearScale(minDB, maxDB, 1000 + 10, 0 + 10, true);
+    const xStep = this._width / (freqs.length - 1);
+    const yScale = linearScale(this.minGain, this.maxGain, this._height, 0, true);
+    const xScale = freq => this._freqToNorm(freq) * this._width;
 
     let selectedFilterPath = ``;
-
     this._selectedFilterDbResponse.forEach((db, index) => {
       const x = index * xStep;
       const y = yScale(db);
@@ -184,7 +208,6 @@ class ScFilter extends ScElement {
     });
 
     let accumulatedFilterPath = ``;
-
     this._accumulatedDbResponse.forEach((db, index) => {
       const x = index * xStep;
       const y = yScale(db);
@@ -193,7 +216,7 @@ class ScFilter extends ScElement {
     });
 
     // close the path
-    accumulatedFilterPath += `1000,1010, 0,1010`;
+    accumulatedFilterPath += `${this._width},${this._height + 10}, 0,${this._height + 10}`;
 
     const node = this._filterNodes[this.editFilterIndex];
     const type = node.type;
@@ -203,29 +226,32 @@ class ScFilter extends ScElement {
 
     // find f1 and f2 from f0 and Q
     // http://www.sengpielaudio.com/calculator-cutoffFrequencies.htm
-    const f1 = freq * (Math.sqrt(1 + 1 / (4 * Q **2)) - 1 / (2 * Q));
-    const f2 = freq * (Math.sqrt(1 + 1 / (4 * Q **2)) + 1 / (2 * Q));
+    this._f1 = freq * (Math.sqrt(1 + 1 / (4 * Q **2)) - 1 / (2 * Q));
+    this._f2 = freq * (Math.sqrt(1 + 1 / (4 * Q **2)) + 1 / (2 * Q));
 
     return html`
-      <svg viewbox="0 0 1000 1000"  preserveAspectRatio="none">
+      <svg
+        @mousedown=${this._onMouseDown}
+      >
         <!-- rulers: freqs: [100, 1000, 10000], gain: 0 -->
         <g>
-          ${[100, 1000, 10000].map(freq => {
+          ${[100, 1000, 10000].map(_freq => {
             return svg`
               <line
                 class="ruler"
-                x1=${this._freqToNorm(freq) * 1000}
-                x2=${this._freqToNorm(freq) * 1000}
+                x1=${xScale(_freq)}
+                x2=${xScale(_freq)}
                 y1="0"
-                y2="1000"
+                y2=${this._height}
               />`;
           })}
+          <!-- ruler: zero gain -->
           <line
             class="ruler"
             x1="0"
-            x2="1000"
-            y1="500"
-            y2="500"
+            x2=${this._width}
+            y1=${this._height / 2}
+            y2=${this._height / 2}
             stroke="steelblue"
             stroke-width="1"
           />
@@ -234,50 +260,82 @@ class ScFilter extends ScElement {
         <polyline class="accumulated-filter" points="${accumulatedFilterPath}" />
         <polyline class="selected-filter" points="${selectedFilterPath}" />
 
-        <g>
-          <!-- freq line -->
-          <line
-            class="filter-control"
-            x1=${this._freqToNorm(freq) * 1000}
-            x2=${this._freqToNorm(freq) * 1000}
-            y1="0"
-            y2="1000"
-          />
-
-          <!-- bandwidth lines -->
-          ${[f1, f2].map(freq => {
-            return svg`
-            <line
-              class="filter-control"
-              x1=${this._freqToNorm(freq) * 1000}
-              x2=${this._freqToNorm(freq) * 1000}
-              y1="0"
-              y2="1000"
-            />`;
-          })}
+        <g class="filter-controls">
+          ${type !== 'lowshelf' && type !== 'highshelf'
+            ? svg`
+              <rect class="filter-params"
+                x=${xScale(this._f1)}
+                y="0"
+                width=${xScale(this._f2) - xScale(this._f1)}
+                height=${this._height}
+              ></rect>`
+            : nothing
+          }
 
           <!-- gain line -->
           ${type === 'lowshelf' || type === 'highshelf' || type === 'peaking'
             ? svg`
               <line
-                class="filter-control"
                 x1="0"
-                x2="1000"
+                x2=${this._width}
                 y1=${yScale(gain) - 1}
                 y2=${yScale(gain) - 1}
               />`
             : nothing
           }
+
+          <!-- freq line -->
+          <line
+            x1=${xScale(freq)}
+            x2=${xScale(freq)}
+            y1="0"
+            y2=${this._height}
+          />
+
+          <!-- bandwidth lines -->
+          ${type !== 'lowshelf' && type !== 'highshelf'
+            ? svg`
+              <line
+                class="q-control f1"
+                x1=${xScale(this._f1)}
+                x2=${xScale(this._f1)}
+                y1="0"
+                y2=${this._height}
+              />
+              <line
+                class="q-control f2"
+                x1=${xScale(this._f2)}
+                x2=${xScale(this._f2)}
+                y1="0"
+                y2=${this._height}
+              />`
+            : nothing
+          }
         </g>
       </svg>
-      <sc-position-surface
-        x-range=${JSON.stringify([0, 1])}
-        y-range=${JSON.stringify([maxDB, minDB])}
-        @input=${this._onInput}
-        clamp
-      >
-      </sc-position-surface>
     `;
+  }
+
+  connectedCallback() {
+    super.connectedCallback();
+
+    // @note - this is important if the compoent is e.g. embedded in another component
+    // this._tabindex = this.getAttribute('tabindex') || 0;
+
+    this._resizeObserver = new ResizeObserver(entries => {
+      const entry = entries[0];
+      const { width, height } = entry.contentRect;
+      this._width = width;
+      this._height = height;
+      this.requestUpdate();
+    });
+
+    this._resizeObserver.observe(this);
+  }
+
+  disconnectedCallback() {
+    this._resizeObserver.disconnect();
+    super.disconnectedCallback();
   }
 
   _normToFreqLin(norm) {
@@ -296,10 +354,10 @@ class ScFilter extends ScElement {
     return 2 * 10 ** exp;
   }
 
-  _normToFreq(freq) {
+  _normToFreq(norm) {
     return this.mode === 'log'
-      ? this._normToFreqLog(freq)
-      : this._normToFreqLin(freq);
+      ? this._normToFreqLog(norm)
+      : this._normToFreqLin(norm);
   }
 
   _freqToNormLin(freq) {
@@ -317,28 +375,80 @@ class ScFilter extends ScElement {
       : this._freqToNormLin(freq);
   }
 
-  _onInput(e) {
-    if (e.detail.value.length === 0) {
-      // @todo - trigger @change event
-      return;
+  _onMouseDown(e) {
+    window.addEventListener('mousemove', this._onMouseMove);
+    window.addEventListener('mouseup', this._onMouseUp);
+    this._requestUserSelectNoneOnBody();
+
+    const $target = e.target;
+
+    if ($target.classList.contains('f1')) {
+      this._editMode = 'f1';
+    } else if ($target.classList.contains('f2')) {
+      this._editMode = 'f2';
+    } else {
+      this._editMode = 'f0';
     }
 
-    // consider only first input (no multitouch)
-    let { x, y: gain } = e.detail.value[0];
+    this._handleEvent(e);
+  }
 
-    // @todo - review
-    x = Math.min(1, Math.max(0, x));
-    const frequency = this._normToFreq(x);
-    // @todo - review
-    gain = Math.min(24, Math.max(gain, -24));
+  _onMouseMove(e) {
+    this._handleEvent(e);
+  }
 
+  _onMouseUp(e) {
+    window.removeEventListener('mousemove', this._onMouseMove);
+    window.removeEventListener('mouseup', this._onMouseUp);
+    this._cancelUserSelectNoneOnBody(e);
+    this._editMode = null;
+  }
 
-    this._filterNodes[this.editFilterIndex].frequency.value = frequency;
+  _handleEvent(e) {
+    // http://www.sengpielaudio.com/calculator-bandwidth.htm
+    // Q = f0 / BW          f0= BW × Q = √ (f1 × f2)
+    // BW = f2 − f1
+
+    const rect = this.getBoundingClientRect();
+    let x = (e.clientX - rect.left) / this._width;
+    x = Math.max(0, Math.min(1, x));
+    let y = (this._height - (e.clientY - rect.top)) / this._height;
+    // y = Math.max(0, Math.min(1, y));
+
+    // gain is updated in every mode
+    const gain = y * (this.maxGain - this.minGain) + this.minGain;
     this._filterNodes[this.editFilterIndex].gain.value = gain;
 
-    this.requestUpdate();
+    switch (this._editMode) {
+      case 'f0': {
+        const frequency = this._normToFreq(x);
+        this._filterNodes[this.editFilterIndex].frequency.value = frequency;
+        break;
+      }
+      // Q = f0 / BW
+      // BW = f2 − f1
+      // f1 = f0^2 / f2 = f2 − BW
+      // f2 = f0^2 / f1 = f1 + BW
+      // @note - Q is not used for highshelf and lowshelf
+      case 'f1': {
+        const f0 = this._filterNodes[this.editFilterIndex].frequency.value;
+        const f1 = Math.min(this._normToFreq(x), f0 - 1e-3); // prevent Infinity
+        const f2 = f0**2 / f1;
+        const Q = f0 / (f2 - f1);
+        this._filterNodes[this.editFilterIndex].Q.value = Q;
+        break;
+      }
+      case 'f2': {
+        const f0 = this._filterNodes[this.editFilterIndex].frequency.value;
+        const f2 = Math.max(this._normToFreq(x), f0 + 1e-3); // prevent Infinity
+        const f1 = f0**2 / f2;
+        const Q = f0 / (f2 - f1);
+        this._filterNodes[this.editFilterIndex].Q.value = Q;
+        break;
+      }
+    }
 
-    // @todo - trigger @input event
+    this.requestUpdate();
   }
 }
 
@@ -348,6 +458,3 @@ if (customElements.get('sc-filter') === undefined) {
 }
 
 export default ScFilter;
-
-// 0 1  2   3    4 5 6
-// 0 10 100 1000 1
