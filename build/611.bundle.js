0f7c90a123fa6c848ab1c46b86dddd6221538ef0
b-ma <benjamin.matuszewski@gmail.com>
2023-12-16 17:01:07 +0000

    Deploying to gh-pages from @ ircam-ismm/sc-components@8c0aee4a064c39991d19e59d01439e26994bfdf9 🚀

---
+++ build/611.bundle.js
@@ -0,0 +1,38 @@
+"use strict";(self.webpackChunk_ircam_sc_components_doc=self.webpackChunk_ircam_sc_components_doc||[]).push([[611],{9611:(e,c,s)=>{s.r(c),s.d(c,{template:()=>r});var t=s(182),l=(s(850),s(4670));const r=t.dy`
+
+<h2>sc-fullscreen</h2>
+
+<sc-code-example language="javascript">${"\nimport { html } from 'lit';\nimport '@ircam/sc-components/sc-fullscreen.js';\n\nconst template = html`\n  <sc-fullscreen></sc-fullscreen>\n`;\n"}</sc-code-example>
+
+<sc-fullscreen
+  id="test-fullscreen"
+></sc-fullscreen>
+
+<h3>Attributes</h3>
+<div>
+  <sc-text>?disabled [=false]</sc-text>
+  <sc-toggle
+    @change=${e=>document.querySelector("#test-fullscreen").disabled=e.detail.value}
+  ></sc-toggle>
+</div>
+
+<h3>Keyboard shortcuts</h3>
+<sc-text class="key">Space</sc-text>
+<sc-text class="key">Enter</sc-text>
+
+<h3>Styling</h3>
+<sc-editor
+  style="width: 500px;"
+  save-button
+  value="\
+#test-fullscreen {
+  width: 30px;
+  height: 30px;
+  border: 1px solid var(--sc-color-primary-3);
+  background-color: var(--sc-color-primary-2);
+}
+  "
+  @change=${e=>(0,l.default)(e.detail.value)}
+></sc-editor>
+
+`}}]);
