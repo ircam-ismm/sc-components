0d477c08efdd3cf73ed78d1f3caba672bd4a533c
b-ma <benjamin.matuszewski@gmail.com>
2025-01-22 16:02:43 +0100

    3.2.2

---
+++ src/utils/is-same-options.js
@@ -0,0 +1,18 @@
+
+import deepEqual from 'deep-equal';
+import { isPlainObject } from '@ircam/sc-utils';
+
+/**
+ * If object check that we have the same key value pairs even in different order
+ * If Array check that we have the same values even in different order
+ */
+export default function isSameOptions(oldOptions, newOptions) {
+  if (
+    (isPlainObject(newOptions) && deepEqual(newOptions, oldOptions))
+    || (Array.isArray(newOptions) && newOptions.slice(0).sort().join(',') === oldOptions.slice(0).sort().join(','))
+  ) {
+    return true;
+  }
+
+  return false;
+}
